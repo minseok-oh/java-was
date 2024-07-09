fcf340cf279c43d4eb8ec3406665e5bae1376014
minseok-oh <minseok.dev@gmail.com>
2024-07-09 14:36:40 +0900

    refactor: 데이터베이스 위치 변경

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,9 +1,11 @@
 package codesquad;
 
+import codesquad.domain.db.UserDatabase;
 import codesquad.server.Server;
 
 public class Main {
     private static final Server server = new Server();
+    public static final UserDatabase userDatabase = new UserDatabase();
 
     public static void main(String[] args) throws Exception {
         server.init();
