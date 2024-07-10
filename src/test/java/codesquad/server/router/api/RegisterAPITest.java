551f7095b5a9ab49344e8e1289efa4caccb43caa
minseok-oh <minseok.dev@gmail.com>
2024-07-10 10:49:38 +0900

    fix: header 구조 변경으로 인한 테스트 코드 수정

--- src/test/java/codesquad/server/router/api/RegisterAPITest.java
+++ src/test/java/codesquad/server/router/api/RegisterAPITest.java
@@ -4,6 +4,7 @@ import codesquad.http.HttpRequest;
 import codesquad.http.HttpResponse;
 import codesquad.http.constant.HttpMethod;
 import codesquad.http.constant.HttpVersion;
+import codesquad.http.element.HttpHeader;
 import codesquad.http.element.HttpHeaders;
 import codesquad.http.element.RequestBody;
 import codesquad.http.element.RequestStartLine;
@@ -19,20 +20,22 @@ import java.util.Map;
 import static codesquad.Main.userDatabase;
 import static org.junit.jupiter.api.Assertions.*;
 
-class CreateUserAPITest {
-    private CreateUserAPI createUserAPI;
+class RegisterAPITest {
+    private RegisterAPI createUserAPI;
 
     @BeforeEach
     void setUp() {
-        createUserAPI = new CreateUserAPI();
+        createUserAPI = new RegisterAPI();
     }
 
     @Test
     @DisplayName("POST 요청을 처리하고 사용자를 데이터베이스에 추가해야 합니다")
     void handleTest() throws URISyntaxException {
         String requestBody = "userId=testUser&nickname=TestUser&password=1234";
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/x-www-form-urlencoded");
+        Map<String, HttpHeader> headers = new HashMap<>();
+        HttpHeader header = new HttpHeader();
+        header.append("application/x-www-form-urlencoded");
+        headers.put("Content-Type", header);
         HttpRequest httpRequest = new HttpRequest(
                 new RequestStartLine(HttpMethod.POST, new URI("/user/create"), HttpVersion.HTTP_1_1),
                 new HttpHeaders(headers),
@@ -40,7 +43,7 @@ class CreateUserAPITest {
         );
 
         HttpResponse httpResponse = createUserAPI.handle(httpRequest);
-        assertNotNull(userDatabase.getUserById("testUser"));
+        assertNotNull(userDatabase.getById("testUser"));
         assertEquals("HTTP/1.1 302 Found\r\n", httpResponse.getResponseStartLine().toString());
     }
 
@@ -48,8 +51,11 @@ class CreateUserAPITest {
     @DisplayName("POST가 아닌 요청일 경우 예외를 발생시켜야 합니다")
     void handleInvalidMethodTest() {
         String requestBody = "userId=testUser&nickname=TestUser&password=1234";
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/x-www-form-urlencoded");
+        Map<String, HttpHeader> headers = new HashMap<>();
+
+        HttpHeader header = new HttpHeader();
+        header.append("application/x-www-form-urlencoded");
+        headers.put("Content-Type", header);
         HttpRequest httpRequest = new HttpRequest(
                 new RequestStartLine(HttpMethod.GET, URI.create("/user/create"), HttpVersion.HTTP_1_1),
                 new HttpHeaders(headers),
