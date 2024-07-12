b12607d71e6d4f00f8a5b88705cb84e7c8f60cc2
minseok-oh <minseok.dev@gmail.com>
2024-07-12 09:56:45 +0900

    feat: custom exception 지원을 위한 변경

--- src/main/java/codesquad/http/constant/HttpStatus.java
+++ src/main/java/codesquad/http/constant/HttpStatus.java
@@ -2,8 +2,11 @@ package codesquad.http.constant;
 
 public enum HttpStatus {
     OK("200", "OK"),
-    PERMANENTLY("301", "Moved Permanently"),
-    FOUND("302", "Found");
+    FOUND("302", "Found"),
+    BAD_REQUEST("400", "Bad Request"),
+    NOT_FOUND("404", "Not Found"),
+    METHOD_NOT_ALLOWED("405", "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR("500", "Internal Server Error");
 
     private final String code;
     private final String message;
@@ -13,14 +16,17 @@ public enum HttpStatus {
         this.message = message;
     }
 
+    @Override
+    public String toString() { return this.code + " " + this.message; }
     public String getCode() { return this.code; }
     public String getMessage() { return this.message; }
 
     public static HttpStatus getStatus(String code) {
         return switch (code) {
             case "200" -> HttpStatus.OK;
-            case "301" -> HttpStatus.PERMANENTLY;
             case "302" -> HttpStatus.FOUND;
+            case "404" -> HttpStatus.NOT_FOUND;
+            case "500" -> HttpStatus.INTERNAL_SERVER_ERROR;
             default -> HttpStatus.OK;
         };
     }
--- src/main/java/codesquad/server/router/api/RegisterAPI.java
+++ src/main/java/codesquad/server/router/api/RegisterAPI.java
@@ -4,6 +4,7 @@ import codesquad.domain.entity.User;
 import codesquad.http.HttpRequest;
 import codesquad.http.HttpResponse;
 import codesquad.http.constant.HttpMethod;
+import codesquad.http.exception.client.Http405Exception;
 import codesquad.server.router.handler.ResourceHandler;
 
 import java.util.Map;
@@ -14,7 +15,7 @@ import static codesquad.domain.entity.User.createUserInfo;
 public class RegisterAPI implements ResourceHandler {
     @Override
     public HttpResponse handle(HttpRequest request) {
-        if (request.getRequestStartLine().method() != HttpMethod.POST) throw new IllegalArgumentException();
+        if (request.getRequestStartLine().method() != HttpMethod.POST) throw new Http405Exception();
 
         Map<String, String> info = createUserInfo(request.getBody().toString());
         userDatabase.append(new User(info.get("userId"), info.get("nickname"), info.get("password")));
