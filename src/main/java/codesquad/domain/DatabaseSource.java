6857d949d49668c8dd299a9a7beae585d1fc96f8
minseok-oh <minseok.dev@gmail.com>
2024-07-17 09:20:42 +0900

    refactor: ddl 관련 파일 생성 후 변경

--- src/main/java/codesquad/domain/DatabaseSource.java
+++ src/main/java/codesquad/domain/DatabaseSource.java
@@ -1,14 +1,24 @@
 package codesquad.domain;
 
+import codesquad.server.processor.message.HttpGenerator;
+import codesquad.server.processor.message.HttpParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Objects;
+
 
 public class DatabaseSource {
     private static final String jdbcURL = "jdbc:h2:tcp://localhost/~/test";
     private static final String username = "sa";
     private static final String password = "";
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseSource.class);
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(jdbcURL, username, password);
@@ -20,33 +30,23 @@ public class DatabaseSource {
     }
 
     public static void createTable() {
-        try (Connection connection = connect(); Statement statement = connection.createStatement()) {
-            String createUserTableSQL = "CREATE TABLE IF NOT EXISTS users (" +
-                    "id INT AUTO_INCREMENT PRIMARY KEY, " +
-                    "userid VARCHAR(255) UNIQUE NOT NULL, " +
-                    "nickname VARCHAR(255) NOT NULL, " +
-                    "password VARCHAR(255) NOT NULL)";
+        try (Connection connection = connect(); Statement statement = connection.createStatement();
+             InputStream dropSQL = DatabaseSource.class.getResourceAsStream("/sql/drop.sql");
+             InputStream createSQL = DatabaseSource.class.getResourceAsStream("/sql/create.sql")) {
 
-            String createPostTableSQL = "CREATE TABLE IF NOT EXISTS posts (" +
-                    "id INT AUTO_INCREMENT PRIMARY KEY, " +
-                    "userid VARCHAR(255), " +
-                    "title VARCHAR(255)," +
-                    "contents TEXT, " +
-                    "FOREIGN KEY (userid) REFERENCES users(userid))";
+            String dropTableSQL = new String(Objects.requireNonNull(dropSQL).readAllBytes());
+            logger.debug(dropTableSQL);
+            statement.execute(dropTableSQL);
 
-            String createCommetTableSQL = "CREATE TABLE IF NOT EXISTS comments (" +
-                    "id INT AUTO_INCREMENT PRIMARY KEY, " +
-                    "postid INT, " +
-                    "userid VARCHAR(255), " +
-                    "contents TEXT, " +
-                    "FOREIGN KEY (postid) REFERENCES posts(id), " +
-                    "FOREIGN KEY (userid) REFERENCES users(userid))";
-
-            statement.execute(createUserTableSQL);
-            statement.execute(createPostTableSQL);
-            statement.execute(createCommetTableSQL);
+            String[] createTableSQL = new String(Objects.requireNonNull(createSQL).readAllBytes()).split("\n");
+            for (String sql: createTableSQL) {
+                logger.debug(sql);
+                statement.execute(sql);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
